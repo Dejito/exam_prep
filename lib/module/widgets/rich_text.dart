3caffad1e7d3127f799bec4373fc303a4fdc3311
dejito <Deerealboy@gmail.com>
2025-05-10 18:21:13 +0100

    completed rich text implementation

--- lib/module/widgets/rich_text.dart
+++ lib/module/widgets/rich_text.dart
@@ -1,22 +1,14 @@
-import 'dart:ffi';
-import 'dart:ui';
-
-import 'package:blackchinx/core/router/navigation_helper.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 
 
 Widget richText({
   required String unformattedText,
   required String formattedText,
-  String unformattedText2 = "",
   Color? unformattedColor = Colors.black,
   required Color? formattedColor,
   double formattedFontSize = 14.0,
-  double unformattedFontSize = 14,
+  double unformattedFontSize = 14.0,
   FontWeight fontWeight = FontWeight.normal
   }) {
   return RichText(
@@ -36,28 +28,7 @@ Widget richText({
             color: formattedColor,
             fontWeight: FontWeight.w600,
           ),
-          // recognizer: TapGestureRecognizer()
-          //   ..onTap = () {
-          //   },
         ),
-        TextSpan(
-          text: unformattedText2,
-          style: TextStyle(color: unformattedColor,
-              fontSize: unformattedFontSize.sp,
-          ),
-        ),
-        // TextSpan(
-        //   text: 'Privacy Policies',
-        //   style: GoogleFonts.poppins(
-        //     fontSize: 12.sp,
-        //     color: const Color(0xFF2957C5),
-        //     fontWeight: FontWeight.bold,
-        //   ),
-        //   recognizer: TapGestureRecognizer()
-        //     ..onTap = () {
-        //       debugPrint('Privacy Policies tapped');
-        //     },
-        // ),
       ],
     ),
   );
