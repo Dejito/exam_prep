768c6dcef91f6bedcbe2cabca425b53e405785d9
dejito <Deerealboy@gmail.com>
2025-05-10 02:05:16 +0100

    implemented app bar

--- lib/module/dashboard/dashboard.dart
+++ lib/module/dashboard/dashboard.dart
@@ -1,40 +1,55 @@
 import 'package:flutter/material.dart';
 
+import '../../constant/app_assets.dart';
+
 class Dashboard extends StatelessWidget {
   const Dashboard({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF79F3A1),
       appBar: AppBar(
+        backgroundColor: const Color(0xFF79F3A1),
+
         title: const Text(
             "Hello,\nAlexandra!"
         ),
         actions: [
-          Stack(
-            alignment: Alignment.centerRight,
-            children: [
-              // Profile Image
-              CircleAvatar(
-                radius: 25,
-                backgroundImage: AssetImage(AppImages.userImage), // Replace with your image
-              ),
-
-              // Overlapping Icon
-              Positioned(
-                right: -10,
-                child: Container(
-                  height: 36,
-                  width: 36,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
+          SizedBox(
+            width: MediaQuery.of(context).size.width * 0.25,
+            child: Stack(
+              alignment: Alignment.centerRight,
+              children: [
+                const Positioned(
+                  left: 8,
+                  child: CircleAvatar(
+                    radius: 25,
+                    backgroundImage: AssetImage(AppImages.userImage), // Or use NetworkImage
+                  ),
+                ),
+                Container(
+                  padding: const EdgeInsets.all(2), // Border width
+                  decoration: const BoxDecoration(
+                    color: Color(0xFF79F3A1), // Border color
                     shape: BoxShape.circle,
                   ),
-                  child: Icon(Icons.grid_view_rounded, size: 20),
+                  child: CircleAvatar(
+                    backgroundColor: Colors.white,
+                    radius: 24,
+                    child: IconButton(
+                      icon: const Icon(Icons.apps),
+                      onPressed: () {
+                        // Handle press
+                      },
+                    ),
+                  ),
                 ),
-              ),
-            ],
+
+              ],
+            ),
           ),
+          Container(width: 20,)
         ],
       ),
     );
