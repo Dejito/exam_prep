f88f53e25c29e8ba410c5d1a5c3934aa307de0a5
dejito <Deerealboy@gmail.com>
2025-05-11 00:06:32 +0100

    started subjectOverviewCard ui implementation

--- lib/module/dashboard/dashboard_widgets.dart
+++ lib/module/dashboard/dashboard_widgets.dart
@@ -35,7 +35,7 @@ Widget scoreBoard({required int score}) {
             ),
             titleText(
               text: "Your recent score",
-              color: Colors.black87,
+              color: Colors.black54,
               fontSize: 20,
               fontWeight: FontWeight.w700,
             )
@@ -52,5 +52,43 @@ Widget scoreBoard({required int score}) {
         ),
       ),
     ],
+  );
+}
+
+
+Widget subjectOverviewCard({
+  required String text,
+  required IconData icon,
+  required double size,
+  required Function() onTap,
+}) {
+  return InkWell(
+    onTap: onTap,
+    splashColor: Colors.grey,
+    child: Container(
+      height: 140,
+      padding: const EdgeInsets.all(16.0),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        borderRadius: BorderRadius.circular(16.0),
+        border: Border.all(color: const Color(0xFFFFD739), width: 1),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          Icon(
+            icon,
+            size: size,
+          ),
+          const SizedBox(height: 12.0),
+          titleText(
+            text: text,
+            fontSize: 13,
+            fontWeight: FontWeight.w500,
+            textAlign: TextAlign.center,
+          ),
+        ],
+      ),
+    ),
   );
 }
