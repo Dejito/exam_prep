3622b5a73aa68f8718fb84e4ea79ada8ba42378e
dejito <Deerealboy@gmail.com>
2025-05-12 09:11:12 +0100

    question and options added

--- lib/models/question_answers_model.dart
+++ lib/models/question_answers_model.dart
@@ -7,10 +7,13 @@ class QuestionAnswersModel {
 
 List<QuestionAnswersModel> questionsRepo = [
   QuestionAnswersModel(
-      question: "",
+    question:
+        "Select the correct judgement about the global economy from the list below",
     options: [
-        "",
-      "",
-  ],
+      "The functioning of the world economy is based on the international division of labor",
+      "The trade balance is the difference between imports and exports for a certain period",
+      "State regulation of foreign trade is carried out exclusively by tariff methods",
+      "International economic relations are carried out in the form of monetary and credit relations",
+    ],
   )
 ];
