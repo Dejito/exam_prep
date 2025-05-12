234ec2a774266461c46421adfbe89e03a1fc7dfc
dejito <Deerealboy@gmail.com>
2025-05-12 09:07:55 +0100

    adding question and options

--- lib/models/question_answers_model.dart
+++ lib/models/question_answers_model.dart
@@ -1,8 +1,16 @@
-
 class QuestionAnswersModel {
   final String question;
   final List<dynamic> options;
 
   QuestionAnswersModel({required this.question, required this.options});
-
 }
+
+List<QuestionAnswersModel> questionsRepo = [
+  QuestionAnswersModel(
+      question: "",
+    options: [
+        "",
+      "",
+  ],
+  )
+];
